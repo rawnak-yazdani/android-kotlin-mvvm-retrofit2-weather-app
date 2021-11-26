ed8b30855b2a240bc6567600207466c24ea036dd
rawnak-yazdani <rawnakasif@gmail.com>
2021-11-26 22:43:57 +0600

    update

--- app/src/main/java/com/rawnak/weatherappmvvm/features/weather_info_show/model/WeatherInfoShowModelImpl.kt
+++ app/src/main/java/com/rawnak/weatherappmvvm/features/weather_info_show/model/WeatherInfoShowModelImpl.kt
@@ -1,7 +1,10 @@
 package com.rawnak.weatherappmvvm.features.weather_info_show.model
 
 import android.content.Context
+import android.util.Log
+import com.google.gson.Gson
 import com.google.gson.GsonBuilder
+import com.google.gson.annotations.SerializedName
 import com.google.gson.reflect.TypeToken
 import com.rawnak.weatherappmvvm.common.RequestCompleteListener
 import com.rawnak.weatherappmvvm.features.weather_info_show.model.data_class.City
@@ -17,23 +20,56 @@ class WeatherInfoShowModelImpl(private val context: Context) : WeatherInfoShowMo
 
     override fun getCityList(callback: RequestCompleteListener<MutableList<City>>) {
         try {
-            val stream = context.assets.open("city_list.json")
-
-            val size =
-                stream.available()   // it returns the number of bytes in corresponding json file
-            val buffer = ByteArray(size)
-            stream.read(buffer)     // it reads the whole json file and sets it to the byte array
-            stream.close()
-            val tContents = String(buffer)      // converting byte array to a string
+//            val stream = context.assets.open("city_list.json")
+//
+//            val size =
+//                stream.available()   // it returns the number of bytes in corresponding json file
+//            val buffer = ByteArray(size)
+//            stream.read(buffer)     // it reads the whole json file and sets it to the byte array
+//            stream.close()
+//            val tContents = String(buffer)      // converting byte array to a string
+            // or doing the above thing with one statement
+            val tContents = context.assets.open("city_list.json").bufferedReader()
+                .use { it.readText() }
             println("tContents ${tContents.replace("\n", "")}")
+            Log.d("tContents", tContents)
 
             // groupListType will hold the Type of the json file
 //            val groupListType = object : TypeToken<ArrayList<City>>() {}.type
             val groupListType = object : TypeToken<MutableList<City>>() {}.type
-            val gson = GsonBuilder().create()
-            val cityList: MutableList<City> = gson.fromJson(tContents, groupListType)
+//            val gson = GsonBuilder().create()
+            val cityList: MutableList<City> = Gson().fromJson(tContents, groupListType)
 
             callback.onRequestSuccess(cityList) //let presenter know the city list
+
+            //////////////////////////////////////////////////////////////////////////////
+
+            // test of Gson
+
+            //-----------------------------------------------------
+            // object to json
+//            val student = Student("Alex", "Rome")
+//            val jsonString = Gson().toJson(student)
+//            println("student $jsonString")
+            //-----------------------------------------------------
+
+            //-----------------------------------------------------
+            // json to object
+            //  raw string
+//            val json1 = """{
+//                |"key_name": "Mark",
+//                |"key_address": {
+//                |               "key_city": "London",
+//                |               "post": "1000"
+//                |               }
+//                |}""".trimMargin()
+//
+////            val jsonObjectType = object : TypeToken<Student>() {}.type
+//            val student: Student = Gson().fromJson(json1, Student::class.java)
+//            println("student ${student.name} ${student.address} ${student.address?.city}")
+            //-----------------------------------------------------
+
+            //////////////////////////////////////////////////////////////////////////////
 
         } catch (e: IOException) {
             e.printStackTrace()
@@ -71,3 +107,16 @@ class WeatherInfoShowModelImpl(private val context: Context) : WeatherInfoShowMo
         })
     }
 }
+
+//data class Student(
+//    @SerializedName("key_name")
+//    var name: String? = null,
+//    @SerializedName("key_address")
+//    var address: Address? = null
+//)
+//
+//data class Address(
+//    @SerializedName("key_city")
+//    var city: String? = null,
+//    var post: String? = null
+//)
